971090e6dc524a67703ad2c26604080c6896a5f1
lucamincuzzi <luca.mincuzzi@outlook.it>
2024-04-09 16:26:18 +0200

    add: Milestone 2 - complete

--- src/org/experis/learning/gestore_eventi/Main.java
+++ src/org/experis/learning/gestore_eventi/Main.java
@@ -8,71 +8,30 @@ public class Main {
         // Inizializzo l'oggetto Evento come null
         Evento evento = null;
 
+        // Inizializzo lo Scanner
+        Scanner eventoScan = new Scanner(System.in);
+
         // Invoco il metodo per creare l'evento
-        evento = creaEvento(evento);
+        evento = creaEvento(evento, eventoScan);
 
         // Se è stato istanziato correttamente chiedo all'utente quali operazioni eseguire sull'evento
         if (evento != null){
-            operazioniEvento(evento);
+            operazioniEvento(evento, eventoScan);
         }
+
+        // Chiudo lo Scanner
+        eventoScan.close();
     }
 
-    private static void operazioniEvento(Evento evento) {
-        boolean uscita = false;
-        Scanner inputScan = new Scanner(System.in);
+    // METODI
 
-        while (!uscita){
-            System.out.println("Cosa si desidera fare? (Inserire il numero associato alla scelta)");
-            System.out.print("1: Prenotare - 2: Disdire - 3: Uscire");
-            int scelta = Integer.parseInt(inputScan.nextLine());
-            switch (scelta){
-                case 1:
-                    if (prenotazione(inputScan, evento)) break;
-                case 2:
-                    if (disdetta(inputScan, evento)) break;
-                case 3:
-                    System.out.println("Arrivederci!");
-                    uscita = true;
-                    break;
-                default:
-                    System.out.println("Scelta invalida: riprovare.");
-                    break;
-            }
-        }
-        inputScan.close();
-    }
-
-    private static boolean disdetta(Scanner inputScan, Evento evento) {
-        try {
-            System.out.print("Quanti posti si desidera disdire?");
-            int posti = Integer.parseInt(inputScan.nextLine());
-            evento.disdici(posti);
-            return true;
-        } catch (IllegalArgumentException e) {
-            System.err.println(e.getMessage());;
-        }
-        return false;
-    }
-
-    private static boolean prenotazione(Scanner inputScan, Evento evento) {
-        try {
-            System.out.print("Quanti posti si desidera prenotare?");
-            int postiPrenotazione = Integer.parseInt(inputScan.nextLine());
-            evento.prenota(postiPrenotazione);
-            return true;
-        } catch (IllegalArgumentException e) {
-            System.err.println(e.getMessage());;
-        }
-        return false;
-    }
-
-    private static Evento creaEvento(Evento evento) {
+    private static Evento creaEvento(Evento evento, Scanner eventoScan) {
         // Fintanto che l'evento non è stato creato con successo
         while(evento == null) {
             // Blocco try-with-resources
-            try (Scanner eventoScan = new Scanner(System.in)) {
+            try {
                 // Ricevo gli input dell'utente
-                System.out.println("Creazione del nuovo evento:");
+                System.out.println("======= Creazione del nuovo evento ===========\n");
                 System.out.print("Titolo: ");
                 String titolo = eventoScan.nextLine();
                 System.out.print("Data - inserire anno: ");
@@ -94,5 +53,80 @@ public class Main {
 
         // Ritorno l'oggetto Evento istanziato con successo
         return evento;
+    }
+
+    private static void operazioniEvento(Evento evento, Scanner inputScan) {
+        boolean uscita = false;
+
+        while (!uscita){
+            System.out.println("\n======== Operazioni Evento =========\nCosa si desidera fare? (Inserire il numero associato alla scelta)");
+            System.out.print("1: Prenotare - 2: Disdire - 3: Uscire ");
+            int scelta = Integer.parseInt(inputScan.nextLine());
+            switch (scelta){
+                // Prenotazione
+                case 1:
+                    // Flag
+                    boolean prenotazioneEffettuata = prenotazione(inputScan, evento);
+
+                    // Fintanto che la prenotazione non viene effettuata
+                    while (!prenotazioneEffettuata){
+                        // Tento la prenotazione
+                        prenotazioneEffettuata = prenotazione(inputScan, evento);
+                    }
+
+                    break;
+                // Disdetta
+                case 2:
+                    // Flag
+                    boolean disdettaEffettuata = disdetta(inputScan, evento);
+
+                    // Fintanto che la disdetta non viene effettuata
+                    while (!disdettaEffettuata){
+                        // Tento la disdetta
+                        disdettaEffettuata = disdetta(inputScan, evento);
+                    }
+
+                    break;
+                // Uscita
+                case 3:
+                    System.out.println("Arrivederci!");
+                    uscita = true;
+                    break;
+                // Input invalido
+                default:
+                    System.out.println("Scelta invalida: riprovare.");
+                    break;
+            }
+        }
+    }
+
+    private static boolean prenotazione(Scanner inputScan, Evento evento) {
+        try {
+            System.out.print("Quanti posti si desidera prenotare? ");
+            int postiPrenotazione = Integer.parseInt(inputScan.nextLine());
+            evento.prenota(postiPrenotazione);
+            infoPosti(evento);
+            return true;
+        } catch (IllegalArgumentException e) {
+            System.err.println(e.getMessage());;
+        }
+        return false;
+    }
+
+    private static boolean disdetta(Scanner inputScan, Evento evento) {
+        try {
+            System.out.print("Quanti posti si desidera disdire? ");
+            int posti = Integer.parseInt(inputScan.nextLine());
+            evento.disdici(posti);
+            infoPosti(evento);
+            return true;
+        } catch (IllegalArgumentException e) {
+            System.err.println(e.getMessage());;
+        }
+        return false;
+    }
+
+    private static void infoPosti(Evento e){
+        System.out.println("------------------\n === Info posti ===\nPosti prenotati: " + e.getPostiPrenotati() + "\nPosti disponibili: " + e.postiDisponibili() + "\n---------------");
     }
 }
